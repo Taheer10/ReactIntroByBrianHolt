945addf23a29ef69219c6d266298457101bc80ad
Tahir <Tahirhussain32145@gmail.com>
2022-07-14 12:26:03 +0545

    After Component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React from "react";
 // import ReactDOM from 'react-dom' --Both works fine(It brings the entire react-dom package)
 import { render } from "react-dom";
-
-const pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement(
     "div",
     {},
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(pet, {
+    React.createElement(Pet, {
       name: "Dog1",
       animal: "Dog",
       breed: "Normal",
     }),
-    React.createElement(pet, {
+    React.createElement(Pet, {
       name: "German shepard",
       animal: "Dog",
       breed: "German",
     }),
-    React.createElement(pet, {
+    React.createElement(Pet, {
       name: "Street Dog",
       animal: "Dog",
       breed: "Street",
