f31079eda7b31679e8b1bca980f85acc5876109c
Tahir Hussain <72434868+Taheer10@users.noreply.github.com>
2022-07-15 11:30:51 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StrictMode } from "react";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <div>
+       
+        <SearchParams />
+      </div> */}
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
